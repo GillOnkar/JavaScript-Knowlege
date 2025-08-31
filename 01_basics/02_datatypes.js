5eda643f304ecf002c9d83fc5cdb051efae0c6e7
GillOnkar <104441357+GillOnkar@users.noreply.github.com>
2025-08-31 07:49:02 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let name = "Onkar Gill"
+let age = 27
+let isLoggedIn = true
+let dateOfBirth;
+
+// number => 2 to the power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalon value
+// undefined => 
+// symbol => unique
+
+//object
+console.log(typeof null)// object
+console.log(typeof dateOfBirth)//undefined
